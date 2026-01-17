d22dc100968057e3beeff9921afea272dea6baa8
ABHISHEK GUPTA <abhishekgupta1864@gmail.com>
2026-01-18 01:13:56 +0530

    implement initial backend setup with environment configuration, database connection, middleware, and response handling

---
+++ backend/src/db/index.js
@@ -0,0 +1,14 @@
+import mongoose from "mongoose";
+import { DB_NAME } from "../constant.js";
+
+const conncet_DB = async () =>{
+  try{
+     const connectionIstance = await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME }`)
+     console.log(`\nMongoDB connected!! DB HOST ${connectionIstance.connection.host}`);
+  }catch(error){
+    console.log("MONGODB connection error:",error)
+    process.exit(1);
+  }
+}
+
+export default conncet_DB;
---
+++ backend/src/index.js
@@ -0,0 +1,14 @@
+import "../env.js";
+import connect_DB from "./db/index.js";
+import {app} from "./app.js";
+
+connect_DB()
+.then(()=>{
+  app.listen(process.env.PORT || 8000,()=>{
+    console.log(`Server is running on http:localhost:${process.env.PORT}`);
+
+  })
+})
+.catch((error)=>{
+  console.log("MongoDB connection failed!!!!",err)
+})

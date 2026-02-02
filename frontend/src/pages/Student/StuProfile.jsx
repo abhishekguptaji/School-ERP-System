9bc3bbc20af88b12e3e02a2a8992023824d45b5d
ABHISHEK GUPTA <abhishekgupta1864@gmail.com>
2026-02-03 03:21:52 +0530

    backcoodi 4

--- frontend/src/pages/Student/StuProfile.jsx
+++ frontend/src/pages/Student/StuProfile.jsx
@@ -1,12 +1,13 @@
+import "./css/StuProfile.css";
 
-
-
-function StuProfile(){
+function StuProfile() {
   return (
-    <>
-    
-    </>
-  )
+    <div className="container-fulid mx-5 mt-2">
+      <div className="profile-card">
+        <h2 className="profile-title">Student Profile</h2>
+      </div>
+    </div>
+  );
 }
 
 export default StuProfile;
